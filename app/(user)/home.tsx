e7fb7edb0b9a6fa57ee2cd929baaf5bdd35ecfdd
talha <atercan0658@gmail.com>
2025-01-16 12:37:05 +0100

    [feature/home] - create total balance, add-funds and exchange buttons

--- app/(user)/home.tsx
+++ app/(user)/home.tsx
@@ -1,56 +1,79 @@
-import { Image, StyleSheet, Platform } from 'react-native';
+import { StyleSheet, Platform, View } from 'react-native';
+import { Link } from 'expo-router';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+import { ThemedScrollView } from '@/components/ThemedScrollView';
+import { IconSymbol } from '@/components/ui/IconSymbol';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
-        <HelloWave />
+    <ThemedScrollView className="p-4">
+      <ThemedView className="gap-2 mb-1">
+        <ThemedText>Total Balance</ThemedText>
       </ThemedView>
+      <ThemedView className="flex-row items-center gap-2">
+        <ThemedText type="title">100 PLN</ThemedText>
+      </ThemedView>
+      <View className='flex-row gap-2 my-3'>
+        <ThemedView className="gap-2 mb-1 rounded-2xl self-start px-1" lightColor='#e5e7eb'>
+          {/* TODO: Change to add-funds when created */}
+          <Link href="/explore">
+            <View className="flex-row items-center py-1.5 px-2">
+              <IconSymbol size={16} name="plus" color="black" />
+              <ThemedText className="ml-2 font-medium">
+                Add Funds
+              </ThemedText>
+            </View>
+          </Link>
+        </ThemedView>
+        <ThemedView className="gap-2 mb-1 rounded-2xl self-start px-2" lightColor='#e5e7eb'>
+          {/* TODO: Change to exchange when created */}
+          <Link href="/explore">
+            <View className="flex-row items-center py-1.5 px-2">
+              <IconSymbol size={16} name="arrow.left.and.right" color="black" />
+              <ThemedText className="ml-2 font-medium">
+                Exchange
+              </ThemedText>
+            </View>
+          </Link>
+        </ThemedView>
+      </View>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 1: Try it</ThemedText>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
+          Edit{" "}
+          <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText>{" "}
+          to see changes. Press{" "}
           <ThemedText type="defaultSemiBold">
             {Platform.select({
-              ios: 'cmd + d',
-              android: 'cmd + m',
-              web: 'F12'
+              ios: "cmd + d",
+              android: "cmd + m",
+              web: "F12",
             })}
-          </ThemedText>{' '}
+          </ThemedText>{" "}
           to open developer tools.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 2: Explore</ThemedText>
         <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
+          Tap the Explore tab to learn more about what's included in this
+          starter app.
         </ThemedText>
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
         <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
         <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
+          When you're ready, run{" "}
+          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText>{" "}
+          to get a fresh <ThemedText type="defaultSemiBold">app</ThemedText>{" "}
+          directory. This will move the current{" "}
+          <ThemedText type="defaultSemiBold">app</ThemedText> to{" "}
           <ThemedText type="defaultSemiBold">app-example</ThemedText>.
         </ThemedText>
       </ThemedView>
-    </ParallaxScrollView>
+    </ThemedScrollView>
   );
 }
 
--- components/ui/Avatar.tsx
+++ components/ui/Avatar.tsx
@@ -4,7 +4,7 @@ export default function Avatar({ name }: { name: string | undefined }) {
   const firstLetter = name?.charAt(0).toUpperCase();
   
   return (
-    <View className="w-8 h-8 rounded-full justify-center items-center ml-2.5 relative z-50 bg-gray-200">
+    <View className="w-8 h-8 rounded-full justify-center items-center ml-2.5 bg-gray-200">
       <Text className="text-black text-base font-bold">{firstLetter}</Text>
     </View>
   );
