aea9bb6a6a80b71f1f4fed9833671e5da957249a
talha <atercan0658@gmail.com>
2025-01-24 15:21:36 +0100

    [refactor/currency-flags] - usd and eur flags are updated

--- components/currency/CircleFlag.tsx
+++ components/currency/CircleFlag.tsx
@@ -4,6 +4,8 @@ import { ThemedView } from "../ThemedView";
 import { ICircleFlagProps } from "@/lib/types/props.types";
 import { getCountriesByCurrency } from "@/utils/getCountry";
 import Toast from "react-native-toast-message";
+import { useMemo } from "react";
+import { Image } from "react-native";
 
 export default function CircleFlag({
   code,
@@ -11,7 +13,12 @@ export default function CircleFlag({
   width = "w-14",
   height = "h-14"
 }: ICircleFlagProps) {
-  const country = getCountriesByCurrency(code);
+  const country = useMemo(() => {
+    const countries = getCountriesByCurrency(code);
+    if (code === "USD") return "US";
+    if (code === "EUR") return code;
+    return countries;
+  }, [code]);
 
   if(!country) {
     Toast.show({
@@ -25,13 +32,21 @@ export default function CircleFlag({
       className={`${width} ${height} rounded-full items-center justify-center overflow-hidden border border-gray-200`}
       lightColor="#111827"
     >
-      {!loading && (
-        <CountryFlag
-          isoCode={country[0]}
-          size={36}
-          style={{ width: "100%", height: "100%" }}
-        />
-      )}
+      {!loading &&
+        country === "EUR" ?
+        (
+          <Image
+            source={require("../../assets/images/eu-uni.png")}
+            className="w-full h-full"
+            resizeMode="cover"
+          />
+        ) : (
+          <CountryFlag
+            isoCode={typeof country === "string" ? country : country[0]}
+            size={36}
+            style={{ width: "100%", height: "100%" }}
+          />
+        )}
     </ThemedView>
   );
 }
